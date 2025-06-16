c25296954bc01b125f3188ab97c243aa3eec5ead
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2025-06-16 17:20:31 -0300

    header has now styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContactListComponent } from './components/contact-list/contact-list.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ContactListComponent],
+  imports: [RouterOutlet,ContactListComponent,HeaderComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
