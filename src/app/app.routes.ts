01bd7924d5a3921e92655d845130a04cc5b82915
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2025-06-27 18:47:43 -0300

    login component now has style and works

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,17 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
 import { ContactListComponent } from './components/contact-list/contact-list.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
     children: [
-      { path: '', redirectTo: 'contacts', pathMatch: 'full' },
-      { path: 'contacts', component: ContactListComponent }
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'contacts', component: ContactListComponent }, 
     ]
-  }
+  },
+  { path: '**', redirectTo: 'login' } 
 ];
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  onSubmit() {
+    this.http.post<any>('http://127.0.0.1:5000/login', {
+      email: this.email,
+      password: this.password
+    }).subscribe({
+      next: (res) => {
+        localStorage.setItem('token', res.token);
+        this.router.navigate(['/contacts']); // o la ruta que uses
+      },
+      error: () => {
+        this.errorMessage = 'Email o contraseña incorrectos.';
+      }
+    });
+  }
+}
+
+
+
+/* import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
+ */
