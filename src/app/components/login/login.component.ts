03f04a30702b5467cc04dcfed4325c8bacb5a25c
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2025-06-30 18:33:49 -0300

    changes in login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, MatIconModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
